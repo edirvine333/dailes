6ce855717cd869012f0c4724d6f647689339416e
Ed Irvine <edwin.irvine@edu.sait.ca>
2021-03-15 20:27:19 -0600

    added more routes to routing module

--- Mar1521/cust_modules/route.js
+++ Mar1521/cust_modules/route.js
@@ -4,8 +4,16 @@ const looper = require('./looper.js')
 
 const router = express.Router()
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.send(looper)
+})
+
+router.get('/api', function (req, res) {
+  res.send('/api')
+})
+
+router.get('/api/route', function (req, res) {
+  res.send('/api/routes')
 })
 
 module.exports = router
@@ -16,8 +24,10 @@ module.exports = router
 //  Line 3:  Declares the variable looper and loads the looper.js module, found in the root director (./)
 //  Line 5:  Declares the variable router and assigns it to the express function using the .Router() class(?)
 //  Line 7:  Using the router function with the .get method, sends the return of the looper function, which is a list of items
-//  Line 7:  Note the "/" in the syntax.  Since this is a module, we treat it as if the home page has been called
+//  Line 7:  The home page route sending the result of the looper function
 //  Line 8:  Using the response function with the .send method, sends the returned list from the looper to the browser
-//  Line 11: Turns this file into a module called router that can be loaded into other Javascript files
+//  Line 11: The /api route, which outputs the string "/api" (Line 12)
+//  Line 15: The /api/route endpoint, which outputs the string "/api/route" (Line 16)
+//  Line 19: Turns this file into a module called router that can be loaded into other Javascript files
 //
 //  ***************************************************************************************************************************
--- Mar1521/server.js
+++ Mar1521/server.js
@@ -7,7 +7,7 @@ const route = require('./cust_modules/route.js')
 
 const app = express()
 
-app.use("/api/route", route)
+app.use('/', route)
 
 app.use(function (req, res) {
   res.status(404)
@@ -27,8 +27,8 @@ app.listen(PORT, function() {
 //  Line 6:  Declares the route variable and loads the custom module route.js, found in the ./cust_modules directory
 //  Line 8:  Declares the app variable and assigned it the express function, providing the .use() and .listen() methods
 //
-//  Line 10: Using the app.use function to define the endpoint '/api/route' and passing it the variable "route" as an argument
-//  Line 10: This points the server to the route.js module when /api/route is used in the URL
+//  Line 10: Using the app.use function to define the endpoint '/' and passing it the variable "route" as an argument
+//  Line 10: This points the server to the route.js module for available routes
 //
 //  Line 12 - 15:  Error handling
 //  Line 17 - 21:  Listening on the defined by the environment variable, or port 3000
